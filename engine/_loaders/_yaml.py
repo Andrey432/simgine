7fe60aaf3df74afb44646c9d449ea8b0f4beffe7
Andrey <zylkovskiy.andrey@mail.ru>
2021-03-27 22:53:41 +0300

    Добавление функционала сохранения файлов для yaml-обработчика

--- engine/_loaders/_yaml.py
+++ engine/_loaders/_yaml.py
@@ -5,15 +5,25 @@ from pathlib import Path
 __all__ = ['YamlLoader']
 
 
+def dumper_decorator(func, tag):
+    def wrapper(dumper, value):
+        return dumper.represent_scalar(tag, func(value))
+    return wrapper
+
+
 class YamlLoader:
     @staticmethod
-    def add_handler(tag: str, handler: Callable[[yaml.SafeLoader, yaml.Node], Any]):
-        yaml.SafeLoader.add_constructor(f'!{tag}', handler)
+    def add_loader(tag: str, loader: Callable[[yaml.SafeLoader, yaml.Node], Any]):
+        yaml.SafeLoader.add_constructor(f'!{tag}', loader)
 
     @staticmethod
-    def load(file: Union[str, Path], default: Any = None) -> Union[list, dict, None]:
+    def add_dumper(tag: str, type_: type, func: Callable[[Any], Any]):
+        yaml.SafeDumper.add_multi_representer(type_, dumper_decorator(func, f'!{tag}'))
+
+    @staticmethod
+    def load(file: Union[str, Path], encoding='utf-8', default=None):
         try:
-            with open(Path(file), 'r', encoding='utf-8') as f:
+            with open(Path(file), 'r', encoding=encoding) as f:
                 return yaml.safe_load(f)
         except FileNotFoundError:
             if default is None:
@@ -21,5 +31,11 @@ class YamlLoader:
             return default
 
     @staticmethod
-    def save():
-        pass
+    def save(file: Union[str, Path], data: Any, mode='w', encoding='utf-8', default=None):
+        try:
+            with open(Path(file), mode, encoding=encoding) as f:
+                yaml.safe_dump(data, f)
+        except FileNotFoundError:
+            if default is None:
+                raise
+            return default
